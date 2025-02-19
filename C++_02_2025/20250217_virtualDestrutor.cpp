462d098d83014a4f4e39350539d17cd8475c3bc8
BexelOJ <bexelo.j@outlook.com>
2025-02-19 04:06:21 -0800

    17_virtual func modified

--- C++_02_2025/20250217_virtualDestrutor.cpp
+++ C++_02_2025/20250217_virtualDestrutor.cpp
@@ -2,17 +2,31 @@
 
 class Base {
 public:
-    Base() { std::cout << "Base class constructor\n"; }
-    virtual ~Base() { std::cout << "Base class destructor\n"; } // Virtual destructor
+    Base() 
+    { 
+        std::cout << "Base class constructor\n"; 
+    }
+    
+    virtual ~Base() 
+    { 
+        std::cout << "Base class destructor\n"; 
+    } // Virtual destructor
 };
 
 class Derived : public Base {
 public:
-    Derived() { std::cout << "Derived class constructor\n"; }
-    ~Derived() { std::cout << "Derived class destructor\n"; }
+    Derived() 
+    { 
+        std::cout << "Derived class constructor\n"; 
+    }
+    ~Derived() 
+    { 
+        std::cout << "Derived class destructor\n"; 
+    }
 };
 
-int main() {
+int main() 
+{
     // Create a Derived object but delete it using a Base pointer
     Base* basePtr1 = new Base();
     Base* basePtr2 = new Derived();
@@ -26,13 +40,27 @@ int main() {
 
 
 /*
+--------------------------------
 Output:
+--------------------------------
+with Virtual Keyword:
 
-Base class constructor
-Base class constructor
-Derived class constructor
-Base class destructor
-Derived class destructor
-Base class destructor
+Base class constructor     // for ptr1 base pointer
+Base class constructor     // for ptr2 base pointer
+Derived class constructor  // for ptr2 derived object
+Base class destructor      // for ptr1 base pointer/object
+Derived class destructor   // for ptr2 derived (object)
+Base class destructor      // for ptr2 base (pointer)
+
+--------------------------------
+withour virtual keyword:
+
+Base class constructor      // for ptr1 for base pointer
+Base class constructor      // for ptr2 for base pointer
+Derived class constructor   // for ptr2 for derived object
+Base class destructor       // for ptr1 base pointer
+Base class destructor       // for ptr2 base pointer
+
+--------------------------------
 */
 
