a8c71c72d6a586ccf2e2c5118048b07e106db162
BexelOJ <bexelo.j@outlook.com>
2025-02-25 06:32:27 -0800

    friend modified, comments added

--- C++_02_2025/20250219_friend.cpp
+++ C++_02_2025/20250219_friend.cpp
@@ -13,11 +13,11 @@ public:
         cout<<"Base Class Parameterized Construtor Called"<<endl;
     }
 
-    friend void friendFunction(Shape &b);
+    friend void friendFunction(Shape &b);  // friend function declaration
 
 };
 
-void friendFunction(Shape &b)
+void friendFunction(Shape &b)  // friend function definition
 {
 
     cout<<"Area: "<<b.length * b.breadth<<endl;
@@ -25,9 +25,10 @@ void friendFunction(Shape &b)
 
 int main()
 {
-Shape b1(3,4);
-Shape *ptr2 = new Shape(5,6);
-friendFunction(*ptr2);
+Shape b1(3,4);      // one object in stack
+Shape *ptr2 = new Shape(5,6);  // another object in heap
+friendFunction(b1);    // passing object 1
+friendFunction(*ptr2);  // passing object 2
 return 0;
 }
 
