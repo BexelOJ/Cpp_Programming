d7597a97a9080cdd84f2b01c994f985199dc3769
BexelOJ <bexelo.j@outlook.com>
2025-02-25 22:37:20 -0800

    static function edited

--- C++_02_2025/20250226_staticMemberFunction.cpp
+++ C++_02_2025/20250226_staticMemberFunction.cpp
@@ -3,16 +3,33 @@
 using namespace std;
 
 class Base{
+public:
+// Static constant member variable
+static const int MAX_VALUE = 100;
 
+// Static function
+static void printMaxValue() {
+    std::cout << "Max Value: " << MAX_VALUE << std::endl;
+}
 };
 
 class Derived: public Base{
 
 };
 
+const int Base::MAX_VALUE;
+
 int main()
 {
+// Accessing static constant directly using the class name
+std::cout << "Max Value: " << Base::MAX_VALUE << std::endl;
 
+// Using static function
+Base::printMaxValue();
+
+Base b1;
+cout<<b1.MAX_VALUE<<endl;
+b1.printMaxValue();
 return 0;
 }
 
