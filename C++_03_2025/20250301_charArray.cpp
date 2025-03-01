eb12c9369f792d27103f48565a1ec91a5b7b0cde
BexelOJ <bexelo.j@outlook.com>
2025-03-01 01:55:46 -0800

    number of digits added

---
+++ C++_03_2025/20250301_charArray.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+char str[] = "110010101001";
+int n=0, one=0, zero=0;
+n= strlen(str);
+for(int i=0;i<n;i++)
+{
+    if(str[i] == '1')
+    {
+        one++;
+    }
+    else
+    {
+     zero++;   
+    }
+}
+cout<<"Number of One : "<<one<<endl;
+cout<<"Number of Zero : "<<zero<<endl;
+return 0;
+}
+
+
