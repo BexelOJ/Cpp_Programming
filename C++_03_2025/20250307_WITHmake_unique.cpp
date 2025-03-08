2d3a2bb57cd03b60e2af90db09de0a3be52c7681
BexelOJ <bexelo.j@outlook.com>
2025-03-08 02:34:12 -0800

    edited added

--- C++_03_2025/20250307_WITHmake_unique.cpp
+++ C++_03_2025/20250307_WITHmake_unique.cpp
@@ -26,61 +26,6 @@ public:
     ~Base() { cout << "Destructor Called" << endl; }
 };
 
-void ObjectInStack()
-{
-    Base b1;  // object b1 in Stack Memory
-    b1.a = 2;
-    b1.b = 5;
-    cout << b1.a << " " << b1.b << endl;
-    
-    Base b2 = b1;  
-    b2.a = 4;
-    b2.b = 7;
-    cout << b2.a << " " << b2.b << endl;
-    cout << b1.a << " " << b1.b << endl;
-    
-    Base b3(b1);
-    b3.a = 8;
-    b3.b = 9;
-    cout << b3.a << " " << b3.b << endl;
-    cout << b2.a << " " << b2.b << endl;
-    cout << b1.a << " " << b1.b << endl;
-
-    Base b4(100);  // object b1 in Stack Memory
-    cout <<b4.a << endl;
-}
-
-void ObjectInHeap()
-{
-    Base* ptr1 = new Base(10, 20);   // object is in Heap Memory with raw pointer 'ptr'
-    cout << ptr1->a << " " << ptr1->b << endl;
-
-    ptr1->a = 30;
-    ptr1->b = 40;
-    cout << ptr1->a << " " << ptr1->b << endl;
-
-    Base* ptr2 = ptr1;  // not an object copy, it's just a pointer copy
-    cout << ptr2->a << " " << ptr2->b << endl;
-
-    ptr2->a = 130;
-    ptr2->b = 140;
-    cout << ptr2->a << " " << ptr2->b << endl;
-    cout << ptr1->a << " " << ptr1->b << endl;
-
-    Base* ptr3 = new Base(*ptr1); // Copy constructor is called to create a new object
-
-    cout << ptr3->a << " " << ptr3->b << endl;
-
-    ptr3->a = 150;
-    ptr3->b = 160;
-    cout << ptr3->a << " " << ptr3->b << endl;
-    cout << ptr2->a << " " << ptr2->b << endl;
-    cout << ptr1->a << " " << ptr1->b << endl;
-
-    delete ptr1;  // both pointer ptr and ptr1 points to same object, so deleteing one pointer is enough.
-    delete ptr3;
-
-}
 
 void UniquePointer()
 {
@@ -100,44 +45,15 @@ void UniquePointer()
 
 int main()
 {
-    ObjectInStack();
-    ObjectInHeap();
+
     UniquePointer();
 
     return 0;
 }
 
 
+/*
 
-
-/* OUTPUT:
-
-
-Default Constructor Called
-2 5
-Copy Constructor Called
-4 7
-2 5
-Copy Constructor Called
-8 9
-4 7
-2 5
-Destructor Called
-Destructor Called
-Destructor Called
-Parameterized Constructor Called
-10 20
-30 40
-30 40
-130 140
-130 140
-Copy Constructor Called
-130 140
-150 160
-130 140
-130 140
-Destructor Called
-Destructor Called
 Parameterized Constructor Called
 Default Constructor Called
 Parameterized Constructor Called
@@ -151,5 +67,3 @@ Destructor Called
 Destructor Called
 
 */
-
-
