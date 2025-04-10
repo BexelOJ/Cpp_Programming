a23e55705a45be849d1e6c046b2f25eaa9a93aab
BexelOJ <bexelo.j@outlook.com>
2025-04-10 05:10:51 -0700

    files added, need to edit later

--- C++_04_2025/20250410_string_07_modifiers.cpp
+++ C++_04_2025/20250410_string_07_modifiers.cpp
@@ -7,33 +7,67 @@ int main()
 string str = "Hello ";
 const char* s = "World";
 str.append(s);
-cout<<str<<endl;
+cout<<str<<endl;  
+// Hello World
 
 string two = "New";
-cout<<two<<endl;
+cout<<two<<endl;  
+// New
 
+// *** assign() *** 
 two.assign("India");
-cout<<two<<endl;
+cout<<two<<endl;  
+// India
 
 two.assign(str + " "+ s);
-cout<<two<<endl;
+cout<<two<<endl;  
+// Hello World World
 
+// *** push_back() *** 
 two.push_back('I');
-cout<<two<<endl;
-two.pop_back();
-cout<<two<<endl;
-two.pop_back();
-cout<<two<<endl;
+cout<<two<<endl;  
+// Hello World WorldI
 
+// *** pop_back() *** 
+two.pop_back();
+cout<<two<<endl; 
+// Hello World World
+
+two.pop_back();
+cout<<two<<endl;  
+// Hello World Worl
+
+// *** insert() *** 
 string str2 = "Developing";
 str2.insert(10," nation ");
-cout<<str2<<endl;
-
-
+cout<<str2<<endl;  
+// Developing nation
+ 
 string str3 = "Developing";
 str3.insert(4," nation ");
-cout<<str3<<endl;
+cout<<"Insert: "<<str3<<endl;  
+// Insert: Deve nation loping
 
+// *** erase() *** 
+str2.erase(10, 8);  // (pos,number)
+cout<<"Erase: "<<str2<<endl;  
+// Erase: Developing
+
+// *** replace() *** 
+string str4 = "added";
+str3.replace(4,4,str4);  // (pos, number, str)
+cout<<"Replace: "<<str3<<endl;  
+// Replace: Deveaddedion loping
+
+
+// *** swap() *** 
+string str6 = "Mc";
+string str7 = "Donalds";
+str6.swap(str7);
+cout<<str6<<endl;  
+// Donalds
+cout<<str7<<endl;  
+// Mc
 
 return 0;
 }
@@ -49,6 +83,10 @@ Hello World WorldI
 Hello World World
 Hello World Worl
 Developing nation 
-Deve nation loping
+Insert: Deve nation loping
+Erase: Developing
+Replace: Deveaddedion loping
+Donalds
+Mc
 
 */
