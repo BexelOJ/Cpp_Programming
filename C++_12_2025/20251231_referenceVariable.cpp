3f3c216daa10a954bca2b397041b670cd4c2d8eb
BexelOJ <bexelo.j@outlook.com>
2025-12-31 00:12:01 -0800

    added

--- C++_12_2025/20251231_referenceVariable.cpp
+++ C++_12_2025/20251231_referenceVariable.cpp
@@ -1,1 +1,12 @@
-i
+#include <iostream>
+
+int main() {
+    int a = 10;
+    int& ref = a;
+    
+    std::cout << "a = " << a << std::endl;
+    std::cout << " ref = " << ref << std::endl;
+
+    return 0;
+}
+
