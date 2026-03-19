bf9a5950ed69ecb7619807ae0c228edef79b896a
BexelOJ <bexelo.j@outlook.com>
2026-03-19 01:39:11 -0700

    files added

---
+++ C++_2026/C++_02_2026/20260209_struct_01.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <string>
+
+struct person {
+    std::string name;
+    int age;
+};
+
+int main() {
+    person p1, p2, p3;
+    p1.name = "Arun";
+    p1.age = 25;
+
+    p2.name = "Madhu";
+    p2.age = 32;
+
+    std::cout << "Name : " << p1.name << std::endl;
+    std::cout << "Age : "  << p1.age << std::endl;
+
+    std::cout << "Name : " << p2.name << std::endl;
+    std::cout << "Age : " << p2.age << std::endl;
+
+    return 0;
+}
+
+
---
+++ C++_2026/C++_02_2026/20260211_deque.cpp
@@ -0,0 +1,77 @@
+﻿#include <iostream>
+#include <deque>
+#include <cstddef> 
+
+int main() {
+    std::deque<int> my_deque;
+    
+    std::deque<int>* dqPtr = new std::deque<int>();
+
+    my_deque.push_back(2);
+    my_deque.push_front(1);
+    my_deque.push_front(3);
+    my_deque.push_front(4);
+    my_deque.push_back(5);
+
+    //std::cout << my_deque[0] << std::endl;
+
+    // Range-based for loop
+    for (auto i : my_deque) {
+        std::cout << i << " ";
+    }
+    std::cout<<std::endl;
+
+    // Indexed loop (explicit positions):
+    for (size_t i = 0; i < my_deque.size(); ++i) {
+        std::cout << my_deque[i] << " ";
+    }
+    std::cout << std::endl;
+
+    // Iterator loop (STL classic):
+    for (auto it = my_deque.begin(); it != my_deque.end(); ++it) {
+        std::cout << *it << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << my_deque.front() << std::endl;
+    std::cout << my_deque.back() << std::endl;
+
+    std::cout << my_deque.size() << std::endl;
+    
+    std::cout << my_deque.empty() << std::endl;
+    my_deque.clear(); // << std::endl;
+    std::cout << my_deque.empty() << std::endl;
+    return 0;
+}
+
+
+/*
+Core Element Operations:
+
+1.) push_back()	Adds element to back 
+​
+2.) push_front()	Adds element to front 
+​
+3.) pop_back()	Removes element from back 
+​
+4.) pop_front()	Removes element from front 
+​
+5.) front()	Accesses first element 
+​
+6.) back()	Accesses last element 
+​
+
+Container Management:
+
+1.) size()	Returns number of elements
+​
+2.) empty()	Checks if deque is empty
+​
+3.) clear()	Removes all elements
+​
+4.) at(index)	Accesses element with bounds checking
+​
+5.) operator[](index)	Accesses element (no bounds checking)
+​
+*/
+
